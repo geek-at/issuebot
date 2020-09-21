b6da0d24a75fcca1643523c3764b9628d2153fa6
Christian Haschek <christian@haschek.at>
2020-09-21 20:13:19 +0200

    fixed wrong ticket number

--- webhook.php
+++ webhook.php
@@ -8,7 +8,7 @@ define('ROOT', dirname(__FILE__));
 $data = json_decode(file_get_contents('php://input'),true);
 
 $action = $data['action'];
-$issue = $data['issue']['id'];
+$issue = $data['issue']['name'];
 $issueurl='http://'.GIT_DOM.'/'.GIT_USER.'/'.GIT_REPO.'/issues/'.$issue;
 
 switch($action)
